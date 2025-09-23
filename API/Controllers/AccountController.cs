ae89d55bd57967801a8d7db651a204ef2e8dd6f1
Youssef-Remah <yremah8@gmail.com>
2025-09-23 20:13:46 +0300

    feat: add UserDto and return it from auth methods with access token
    - Added a UserDto class to API/DTOs
    - Modified the Login and Register methods to return a UserDto containing access token
    - Added a token key to the development configuration settings

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,15 +3,16 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class AccountController(AppDbContext dbContext) : BaseApiController
+    public class AccountController(AppDbContext dbContext, ITokenService tokenService) : BaseApiController
     {
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await EmailExists(registerDto.Email)) return BadRequest("Email Taken");
 
@@ -29,12 +30,18 @@ namespace API.Controllers
 
             await dbContext.SaveChangesAsync();
 
-            return user;
+            return new UserDto()
+            {
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                Id = user.Id,
+                Token = tokenService.CreateToken(user)
+            };
         }
 
 
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             AppUser? user = await dbContext.Users.SingleOrDefaultAsync(u => u.Email.ToLower() == loginDto.Email.ToLower());
 
@@ -49,7 +56,13 @@ namespace API.Controllers
                 if (user.PasswordHash[i] != computedPassword[i]) return Unauthorized("Invalid Password.");
             }
 
-            return user;
+            return new UserDto()
+            {
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                Id = user.Id,
+                Token = tokenService.CreateToken(user)
+            };
         }
 
         private async Task<bool> EmailExists(string email)
